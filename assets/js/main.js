7ade0c87b6b9a6ed597ab9d59e9c15a02507b0f1
alimranhung <42710065+alimranhung@users.noreply.github.com>
2018-09-10 12:22:47 +0600

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+jQuery(document).ready(function($){
+
+     $("html").perfectScrollbar({
+     	theme:'green'
+     });
+     $(".section-bg-area").perfectScrollbar({
+     	theme:'red'
+     });
+        
+});
